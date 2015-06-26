609a28249b38450d93ff74dbfa69cdc2dff41dcb
wutongke <ximu470791413@163.com>
2015-06-26 16:36:06 +0800

    pull

---
+++ src/com/cpstudio/zhuojiaren/ui/MyZhuoBiActivity.java
@@ -0,0 +1,19 @@
+package com.cpstudio.zhuojiaren.ui;
+
+import com.cpstudio.zhuojiaren.R;
+import com.cpstudio.zhuojiaren.R.layout;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MyZhuoBiActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_my_zhuo_bi);
+	}
+
+
+}
