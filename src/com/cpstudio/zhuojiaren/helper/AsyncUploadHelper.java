50f2f6d0f71d846040cbaa0d7c21bafd8e1d0fe1
wutongke <ximu470791413@163.com>
2015-07-30 15:33:17 +0800

    asdas

--- src/com/cpstudio/zhuojiaren/helper/AsyncUploadHelper.java
+++ src/com/cpstudio/zhuojiaren/helper/AsyncUploadHelper.java
@@ -7,9 +7,13 @@ import java.util.concurrent.CountDownLatch;
 
 import org.json.JSONObject;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
 import android.os.AsyncTask;
 
+import com.cpstudio.zhuojiaren.R;
 import com.qiniu.android.http.ResponseInfo;
 import com.qiniu.android.storage.UpCompletionHandler;
 import com.qiniu.android.storage.UpProgressHandler;
@@ -17,23 +21,48 @@ import com.qiniu.android.storage.UploadManager;
 import com.qiniu.android.storage.UploadOptions;
 
 public class AsyncUploadHelper extends
-		AsyncTask<List<String>, Integer,List<String>> {
+		AsyncTask<List<String>, Integer, List<String>> {
+
+	private ProgressDialog mDialog;
+
 	UploadManager uploadManager;
 	private UploadCompleteCallback mCallback = null;
 	ArrayList<String> keysList = new ArrayList<String>();
-   String token;
-	public AsyncUploadHelper(String  token,UploadCompleteCallback mCallback) {
+	String token;
+	Context mContext = null;
+
+	public AsyncUploadHelper(Context context, String token,
+			UploadCompleteCallback mCallback) {
 		super();
+		this.mContext = context;
 		this.mCallback = mCallback;
 		this.token = token;
 	}
 
 	@Override
+	protected void onPreExecute() {
+		// TODO Auto-generated method stub
+
+		if (mContext != null) {
+			mDialog = new ProgressDialog(mContext);
+			mDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			mDialog.setTitle(R.string.info63);
+			mDialog.setMessage(mContext.getString(R.string.infofile));
+			mDialog.setIcon(R.drawable.ico_alert);
+			mDialog.setIndeterminate(false);
+			mDialog.setCanceledOnTouchOutside(false);
+
+			mDialog.show();
+		}
+
+	}
+
+	@Override
 	protected List<String> doInBackground(List<String>... files) {
 		// TODO Auto-generated method stub
-		
-		if(token==null)
-			  token = "gqyn9mD9OEVHoayK16ivmeCMcUgLNxVnxIjcrGCm:i7rT4MfI_Wg2guWBLyOV5CJQmYw=:eyJzY29wZSI6InpodW90ZXN0IiwiZGVhZGxpbmUiOjE0MzgyMzk1ODJ9";
+
+		if (token == null)
+			token = "gqyn9mD9OEVHoayK16ivmeCMcUgLNxVnxIjcrGCm:i7rT4MfI_Wg2guWBLyOV5CJQmYw=:eyJzY29wZSI6InpodW90ZXN0IiwiZGVhZGxpbmUiOjE0MzgyMzk1ODJ9";
 		// 真实环境需要替换 final String token = uploadFileToken;
 		List<String> filePathsList = files[0];
 		final List<String> keys = Collections
@@ -48,12 +77,17 @@ public class AsyncUploadHelper extends
 				@Override
 				public void complete(String key, ResponseInfo arg1,
 						JSONObject arg2) {
-					//arg1:{ResponseInfo:1438236401224267,status:401, reqId:RmcAAAf1X1xjpPUT, xlog:UP/401, xvia:null, host:upload.qiniu.com, path:/mkblk/3226274, ip:106.38.227.5, port:-1, duration:1.412000 s, time:1438236402, sent:42,error:token is expired 72273 seconds}
-//					status为200则正常
-					
+					// arg1:{ResponseInfo:1438236401224267,status:401,
+					// reqId:RmcAAAf1X1xjpPUT, xlog:UP/401, xvia:null,
+					// host:upload.qiniu.com, path:/mkblk/3226274,
+					// ip:106.38.227.5, port:-1, duration:1.412000 s,
+					// time:1438236402, sent:42,error:token is expired 72273
+					// seconds}
+					// status为200则正常
+
 					// TODO Auto-generated method stub
-					//token过期时会返回null
-					if (arg2 != null) {
+					// token过期时会返回null
+					if (arg1.isOK() && arg2 != null) {
 						String s = arg2.optString("key", "");
 						if (s != null)
 							keys.add(s);
@@ -79,6 +113,8 @@ public class AsyncUploadHelper extends
 	@Override
 	protected void onPostExecute(List<String> result) {
 		// TODO Auto-generated method stub
+		if (mDialog != null)
+			mDialog.dismiss();
 		if (mCallback != null)
 			mCallback.onReturn(result);
 	}
