cffe6b52ca36aa615dff6227e4aded82dad18d8a
wutongke <ximu470791413@163.com>
2015-09-27 23:32:47 +0800

    aa

--- src/com/cpstudio/zhuojiaren/imageloader/LoadImage.java
+++ src/com/cpstudio/zhuojiaren/imageloader/LoadImage.java
@@ -86,6 +86,7 @@ public class LoadImage {
 	public void addTask(String url, ImageView img) {
 		if (null != url && !url.equals("")) {
 			Bitmap bitmap = memoryCache.getBitmapFromCache(url);
+			img.setTag(url);
 			if (bitmap != null) {
 				img.setImageBitmap(ImageRectUtil.toRoundCorner(bitmap, round));
 			} else {
