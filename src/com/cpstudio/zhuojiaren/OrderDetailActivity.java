537688e78aa5e17f86f9b20ffaf3095d8ded313e
wutongke <ximu470791413@163.com>
2015-11-21 10:56:53 +0800

    wdwd

--- src/com/cpstudio/zhuojiaren/OrderDetailActivity.java
+++ src/com/cpstudio/zhuojiaren/OrderDetailActivity.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -14,8 +16,13 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 import com.cpstudio.zhuojiaren.adapter.OrderListItemGoodsListAdapter;
+import com.cpstudio.zhuojiaren.helper.JsonHandler;
+import com.cpstudio.zhuojiaren.helper.ZhuoConnHelper;
 import com.cpstudio.zhuojiaren.model.GoodsVO;
+import com.cpstudio.zhuojiaren.model.MsgTagVO;
+import com.cpstudio.zhuojiaren.model.OrderVO;
 import com.cpstudio.zhuojiaren.ui.GoodsDetailLActivity;
+import com.cpstudio.zhuojiaren.util.CommonUtil;
 import com.cpstudio.zhuojiaren.widget.NestedListView;
 
 public class OrderDetailActivity extends BaseActivity {
@@ -41,8 +48,9 @@ public class OrderDetailActivity extends BaseActivity {
 	@InjectView(R.id.rlCmt)
 	View rlCmt;
 	String orderId = null;
-	List<GoodsVO> goodsList = new ArrayList<GoodsVO>();
+	// List<GoodsVO> goodsList = new ArrayList<GoodsVO>();
 	OrderListItemGoodsListAdapter mAdapter;
+	private ZhuoConnHelper mConnHelper = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -51,32 +59,88 @@ public class OrderDetailActivity extends BaseActivity {
 		ButterKnife.inject(this);
 		initTitle();
 		title.setText(R.string.title_activity_order_detail);
-
+		mConnHelper = ZhuoConnHelper.getInstance(getApplicationContext());
 		orderId = getIntent().getStringExtra("orderId");
 
-		mAdapter = new OrderListItemGoodsListAdapter(this, goodsList);
-
-		lvGoods.setAdapter(mAdapter);
-		lvGoods.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				// TODO Auto-generated method stub
-				Intent intent = new Intent(OrderDetailActivity.this,GoodsDetailLActivity.class);
-//				intent.putExtra("goodsId", goodsList.get(position).getGid());
-				OrderDetailActivity.this.startActivity(intent);
-			}
-				
-		});
 		loadData();
+	}
+
+	private Handler mUIHandler = new Handler() {
+		@SuppressWarnings("unchecked")
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case MsgTagVO.DATA_LOAD: {
+				OrderVO item = null;
+				if (msg.obj instanceof OrderVO) {
+					item = (OrderVO) msg.obj;
+				} else {
+					if (msg.obj != null && !msg.obj.equals("")) {
+						JsonHandler nljh = new JsonHandler((String) msg.obj,
+								getApplicationContext());
+						item = nljh.parseOrderVO();
+						if (item != null) {
+							// infoFacade.update(list);
+							updateData(item);
+						}
+					}
+				}
+				break;
+			}
+			}
+		}
+
+	};
+
+	private void updateData(OrderVO item) {
+		// TODO Auto-generated method stub
+		if (item == null)
+			return;
+		String[] statusArray = getResources().getStringArray(
+				R.array.order_status);
+		if (item.getStatus() >= 0 && item.getStatus() < statusArray.length)
+			setText(tvStatus, statusArray[item.getStatus()]);
+		setText(tvZhuobi, item.getTotalZhuobi());
+
+		setText(tvAddress, item.getReceiverAddr());
+		setText(tvReceiver, item.getReceiver());
+		setText(tvDealTime, item.getReceipttime());
+		setText(tvOrderNum, item.getBillNo());
+		setText(tvPhoneNum, item.getPhone());
+
+		final List<GoodsVO> goodsList = item.getBuyGoods();
+		if (goodsList != null) {
+			mAdapter = new OrderListItemGoodsListAdapter(this, goodsList);
+			lvGoods.setAdapter(mAdapter);
+			lvGoods.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					// TODO Auto-generated method stub
+					Intent intent = new Intent(OrderDetailActivity.this,
+							GoodsDetailLActivity.class);
+					intent.putExtra("goodsId", goodsList.get(position)
+							.getGoodsId());
+					OrderDetailActivity.this.startActivity(intent);
+				}
+			});
+		}
+	}
+
+	private void setText(TextView tv, String value) {
+		if (tv == null || value == null)
+			return;
+		tv.setText(value);
 	}
 
 	private void loadData() {
 		// TODO Auto-generated method stub
-		for (int i = 0; i < 3; i++)
-			goodsList.add(new GoodsVO());
-		mAdapter.notifyDataSetChanged();
+
+		if (CommonUtil.getNetworkState(getApplicationContext()) == 2) {
+		} else {
+			mConnHelper.getOrderDetail(mUIHandler, MsgTagVO.DATA_LOAD, orderId);
+		}
 	}
 
 }
--- src/com/cpstudio/zhuojiaren/model/OrderVO.java
+++ src/com/cpstudio/zhuojiaren/model/OrderVO.java
@@ -1,83 +1,152 @@
 package com.cpstudio.zhuojiaren.model;
 
 import java.util.List;
+
 /**
  * order goods
+ * 
  * @author lef
- *
+ * 
  */
 public class OrderVO {
-	//商品
-	private List<GoodsVO> goodsList;
-	//订单号
-	private String orderNum;
-	//时间
-	private String orderTime;
-	
-	private UserVO user;
-	
-	private LocateVO locate;
+	// 商品
+	private List<GoodsVO> buyGoods;
+	// 订单号
+	private String billNo;
+	// 发票信息
+	private String invoice;
+	// 给商家留言
+	private String message;
 
-	
-//	lz,订单商品总数
-	private int totalCount=0;
-	private String totalPrice="122.43";
-	
-	
-	public String getTotalPrice() {
-		return totalPrice;
+	private String userid;
+	// (倬币总价)
+	private String totalZhuobi;
+	// <int> (订单状态 0-未支付 1-已成功支付 2-支付失败 3-订单取消 4-卖家已发货 5-买家已收货) ,
+	private int status;
+	// 订单生成时间
+	private String gentime;
+	// 订单支付时间
+	private String paytime;
+	// 订单发货时间
+	private String sendtime;
+	// 订单收货时间
+	private String receipttime;
+	// 收货人
+	private String receiver;
+	// 收货地址
+	private String receiverAddr;
+	// 收货人电话
+		private String phone;
+
+	public String getPhone() {
+			return phone;
+		}
+
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+
+	public String getReceiver() {
+		return receiver;
 	}
 
-	public void setTotalPrice(String totalPrice) {
-		this.totalPrice = totalPrice;
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
 	}
 
-	public int getTotalCount() {
-		return totalCount;
+	public String getReceiverAddr() {
+		return receiverAddr;
 	}
 
-	public void setTotalCount(int totalCount) {
-		this.totalCount = totalCount;
+	public void setReceiverAddr(String receiverAddr) {
+		this.receiverAddr = receiverAddr;
 	}
 
-	public List<GoodsVO> getGoodsList() {
-		return goodsList;
+	public List<GoodsVO> getBuyGoods() {
+		return buyGoods;
 	}
 
-	public String getOrderNum() {
-		return orderNum;
+	public void setBuyGoods(List<GoodsVO> buyGoods) {
+		this.buyGoods = buyGoods;
 	}
 
-
-	public String getOrderTime() {
-		return orderTime;
+	public String getBillNo() {
+		return billNo;
 	}
 
-	public UserVO getUser() {
-		return user;
+	public void setBillNo(String billNo) {
+		this.billNo = billNo;
 	}
 
-	public LocateVO getLocate() {
-		return locate;
+	public String getInvoice() {
+		return invoice;
 	}
 
-	public void setGoodsList(List<GoodsVO> goodsList) {
-		this.goodsList = goodsList;
+	public void setInvoice(String invoice) {
+		this.invoice = invoice;
 	}
 
-	public void setOrderNum(String orderNum) {
-		this.orderNum = orderNum;
+	public String getMessage() {
+		return message;
 	}
 
-	public void setOrderTime(String orderTime) {
-		this.orderTime = orderTime;
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
-	public void setUser(UserVO user) {
-		this.user = user;
+	public String getUserid() {
+		return userid;
 	}
 
-	public void setLocate(LocateVO locate) {
-		this.locate = locate;
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getTotalZhuobi() {
+		return totalZhuobi;
+	}
+
+	public void setTotalZhuobi(String totalZhuobi) {
+		this.totalZhuobi = totalZhuobi;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getGentime() {
+		return gentime;
+	}
+
+	public void setGentime(String gentime) {
+		this.gentime = gentime;
+	}
+
+	public String getPaytime() {
+		return paytime;
+	}
+
+	public void setPaytime(String paytime) {
+		this.paytime = paytime;
+	}
+
+	public String getSendtime() {
+		return sendtime;
+	}
+
+	public void setSendtime(String sendtime) {
+		this.sendtime = sendtime;
+	}
+
+	public String getReceipttime() {
+		return receipttime;
+	}
+
+	public void setReceipttime(String receipttime) {
+		this.receipttime = receipttime;
 	}
 }
